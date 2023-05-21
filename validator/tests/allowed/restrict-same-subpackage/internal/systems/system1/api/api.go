da6be5f4b075a6e2bb8e0f33533c47380421dd60
Mathieu Durand <1435391+matdurand@users.noreply.github.com>
2023-05-21 14:18:14 -0400

    chore: switch to another org

--- validator/tests/allowed/restrict-same-subpackage/internal/systems/system1/api/api.go
+++
@@ -1,7 +0,0 @@
-package api
-
-import "github.com/quantumcycle/go-import-checks/validator/tests/allowed/restrict-same-subpackage/internal/systems/system1/domain"
-
-func ApiFn() {
-	domain.DomainFn()
-}
